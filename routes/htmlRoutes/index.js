dbb9bab1148eea7450861606886f9a19968af71d
jess-smith49 <js.jessicasmith774@gmail.com>
2020-11-14 13:37:56 -0600

    started delete functionality, working on showing HTML pages

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -7,11 +7,16 @@ router.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
+//deleting notes
+/*router.delete('/notes/:id', (req, res) =>{
+    const
+})*/
 
 //homepage route//any route//DEFAULT ROUTE for index.html PLACED AT BOTTOM
 router.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
+
 
 
 //getting notes and stuff
