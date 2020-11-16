2591edd93e4dbb202dedd173d72d0d6db2635f47
jess-smith49 <js.jessicasmith774@gmail.com>
2020-11-16 14:26:24 -0600

    pages displaying:

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,6 +2,10 @@ const path = require('path');
 const router = require('express').Router();
 
 
+router.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, '../../public/index.html'))
+});
+
 //url browser requesting
 router.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
